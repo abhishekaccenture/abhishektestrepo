be1671fc7369569245d50d162343203ad83f7319
abhishekaccenture <abhishek.a.dasgupta@accenture.com>
2017-07-24 16:10:25 +0530

    Update import_eventServer.py

--- data/import_eventServer.py
+++ data/import_eventServer.py
@@ -32,7 +32,7 @@ def import_events(client, file):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
     description="Import twitter data for sentiment analysis")
-  parser.add_argument('--access_key', default='juOu9GG4xspJHhBXz-DKTR8DrvUw-O9MGNi7d3CYg1FCiXA9p7-cDaER2hZlUit3')
+  parser.add_argument('--access_key', default='Jd1embNZ48CWBi2-1AsoOVzwKajeorAHzpEfjmSgVFqgNgXS5xfL7CuutXREWtBi')
   parser.add_argument('--url', default="https://adg-pio-eventserver.herokuapp.com")
   parser.add_argument('--file', default="./data/sample.csv")
 
